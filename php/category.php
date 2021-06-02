77e2ea927765761c6f86aac646e15fad9e34a87e
mylaptop <hieuvanphan275@gmail.com>
2021-06-03 00:21:28 +0700

    hieu

---
+++ php/category.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/css/table.css">
+    <link rel="stylesheet" href="../bootstrap%203.4.1/css/bootstrap.css">
+    <title>Table</title>
+</head>
+<body>
+<h1>Danh sách sản phẩm</h1>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Price</th>
+        <th scope="col">Funcion</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Ip 12 Pro Max</td>
+        <td>100</td>
+        <td>
+            <a class="btn btn-primary" href="addproduct.php" role="button">Add</a>
+            <a class="btn btn-primary" href="edit.php" role="button">Edit</a>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Apple Watch</td>
+        <td>200</td>
+        <td> <a class="btn btn-primary" href="addproduct.php" role="button">Add</a>
+            <a class="btn btn-primary" href="edit.php" role="button">Edit</a></td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>MacBook Pro 2021</td>
+        <td>20</td>
+        <td> <a class="btn btn-primary" href="addproduct.php" role="button">Add</a>
+            <a class="btn btn-primary" href="edit.php" role="button">Edit</a></td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <td>Apple Watch Seri 6</td>
+        <td>200</td>
+        <td> <a class="btn btn-primary" href="addproduct.php" role="button">Add</a>
+            <a class="btn btn-primary" href="edit.php" role="button">Edit</a></td>
+    </tr>
+    <tr>
+        <th scope="row">5</th>
+        <td>Air Tag</td>
+        <td>1000</td>
+        <td> <a class="btn btn-primary" href="addproduct.php" role="button">Add</a>
+            <a class="btn btn-primary" href="edit.php" role="button">Edit</a></td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
---
+++ php/edit.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/css/addpr.css">
+    <link rel="stylesheet" href="../bootstrap%203.4.1/css/bootstrap.css">
+    <title>Table</title>
+</head>
+<body>
+<h1> Update Sản Phẩm</h1>
+<div class="container">
+    <div class="row">
+        <div class="row">
+            <div class="col-md-4">
+                <label style="font-weight: 500" for="fname">Name Product</label><br/>
+                <input type="text" id="fname" name="fname" required><br/>
+            </div>
+            <div class="col-md-4">
+                <label style="font-weight: 500" for="middle">Price</label><br/>
+                <input type="number" id="middle" name="fname"><br/>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="back">
+            <a href="category.php"><< Back </a>
+        </div>
+        <div class="button">
+            <button type="button" onclick="alert('Sua thanh cong')">Update</button>
+
+        </div>
+</body>
